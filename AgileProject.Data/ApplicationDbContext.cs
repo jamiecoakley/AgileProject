761b21ffd611afd6b5c225a5701e43dc0decbfe8
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-29 18:20:08 -0500

    First migration, databases created

--- AgileProject.Data/ApplicationDbContext.cs
+++ AgileProject.Data/ApplicationDbContext.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using AgileProject.Data.Entities;
 
 namespace AgileProject.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        public DbSet<GameEntity> Games { get; set; }
+
+        public DbSet<GameSystemEntity> GameSystems { get; set; }
     }
 }
---
+++ AgileProject.Data/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgileProject.Data.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+    }
+}
