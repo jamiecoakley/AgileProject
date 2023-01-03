de464e2825fba3bade5a84703dd5c3055940cdea
jamiecoakley <jamiemcoakley@gmail.com>
2023-01-03 15:49:12 -0500

    Says I still have changes to push that I thought were pushed... let's try this again!

--- AgileProject.Models/GameViewModels/GameListItem.cs
+++ AgileProject.Models/GameViewModels/GameListItem.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace AgileProject.Models.GameViewModels
 {
     public class GameListItem
     {
-        
+        public string Title { get; set; }
+        //public GameSystemEntity GameSystem { get; set; }
+        //public GenreEntity GameGenre {get;set;}
     }
 }
