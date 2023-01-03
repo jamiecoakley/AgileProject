ebd27c3af127c251d3a69c4ba1d3350fc3a57ab4
Darneisha Miller <darneisham20@gmail.com>
2023-01-03 14:57:52 -0500

    Game System Models: Create, Details, ListItem, and Update

--- AgileProject.Models/GameSystemModels/GameSystemListItem.cs
+++ AgileProject.Models/GameSystemModels/GameSystemListItem.cs
@@ -7,6 +7,8 @@ namespace AgileProject.Models.GameSystemModels
 {
     public class GameSystemListItem
     {
-        
+        public int Id { get; set; }
+        public string SystemName { get; set; }
+        public DateTimeOffset SystemNameCreated { get; set; }
     }
 }
