c7d638d81331bac29a576167edcabab1ded1966e
Darneisha Miller <darneisham20@gmail.com>
2023-01-03 16:29:26 -0500

    Game Systems, IGameSystems in Services Final Update

--- AgileProject.Services/GameSystem/GameSystems.cs
+++ AgileProject.Services/GameSystem/GameSystems.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using AgileProject.Data;
 using AgileProject.Data.Entities;
-using AgileProject.Models;
 using AgileProject.Models.GameSystemModels;
 using AgileProject.Services.GameSystems;
 
@@ -15,35 +14,59 @@ namespace AgileProject.Services.GameSystem
     public class GameSystems : IGameSystems
     {
         private readonly ApplicationDbContext _dbContext;
-        public GameSystems(ApplicationDbContext dbContext)
+        public GameSystems(IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public async Task<bool> CreateGameSystemAsync(GameSystemCreate request)
+        public async Task<bool> CreateGameSystemsAsync(GameSystemCreate request)
         {
             var GameSystemEntity = new GameSystemEntity
             {
                 SystemName = request.SystemName
             };
 
-        _dbContext.GameSystems.Add(GameSystemEntity);
+            _dbContext.GameSystems.Add(GameSystemEntity);
 
-        var numberOfChanges = await _dbContext.SaveChangesAsync();
-        return numberOfChanges == 1;
+            var numberOfChanges = await _dbContext.SaveChangesAsync();
+            return numberOfChanges == 1;
+
+            throw new NotImplementedException();
         }
 
-        // public async Task<IEnumerable<GameSystemListItem>> GetAllGameSystemsAsync()
-        // {
-        //     var gameSystems = await _dbContext.GameSystems
-        //         .Where(entity => entity.Id == 1)
-        //         .Select(entity => new GameSystemListItem)
-        //         {
-                    
-        //         }
-        //         .ToListAsync();
-            
-        //     return GameSystems;
-        // }
+        public async Task<bool> DeleteGameSystemAsync(int GameSystemId)
+        {
+            var GameSystemEntity = await _dbContext.GameSystems.FindAsync(GameSystemId);
+
+            if (GameSystemEntity?.GameSystemId != 1)
+                return false;
+
+            _dbContext.GameSystems.Remove(GameSystemEntity);
+            return await _dbContext.SaveChangesAsync() == 1;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<GameSystemListItem>> GetAllGameSystemsAsync()
+        {
+            var gameSystems = await _dbContext.GameSystems
+            .Select(entity => new GameSystemListItem
+            {
+                SystemName = entity.SystemName,
+            })
+            .ToListAsync();
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateGameSystemAsync(GameSystemUpdate request)
+        {
+            var GameSystemEntity = await _dbContext.GameSystems.FindAsync(request.Id);
+
+            GameSystemEntity.SystemName = request.SystemName;
+
+            var numberOfChanges = await _dbContext.SaveChangesAsync();
+            throw new NotImplementedException();
+        }
     }
 }
